388920ed1c36cd4fac1e67dc9615b171c87aea32
Chupakabra0 <rootalfa4@gmail.com>
2022-04-10 13:01:46 +0300

    Added new console arg

--- ImageProcessingLab2/src/Main.cpp
+++ ImageProcessingLab2/src/Main.cpp
@@ -8,7 +8,9 @@ std::unique_ptr<argparse::ArgumentParser> InitMain(const int argc, char** argv) 
 
     auto parser = std::make_unique<argparse::ArgumentParser>();
     parser->add_argument("-f", "--filepath")
-        .required().help("specify the input image file");
+        .required().help("specify the input image file"); 
+    parser->add_argument("-l", "--logarithm")
+        .default_value(1).scan<'i', int>().help("SAMPLE LOG TEXT");
 
     try {
         parser->parse_args(argc, argv);
@@ -25,8 +27,7 @@ int main(const int argc, char** argv) {
         auto parser   = InitMain(argc, argv);
         auto filePath = parser->get<std::string>("-f");
         auto image = std::make_shared<cv::Mat>(cv::imread(filePath, cv::ImreadModes::IMREAD_GRAYSCALE));
-        //auto image    = std::make_shared<cv::Mat>(cv::imread(filePath, cv::ImreadModes::IMREAD_ANYCOLOR | cv::ImreadModes::IMREAD_ANYDEPTH));
-        std::clog << std::format("Open Size: {} x {}\n", image->rows, image->cols);
+        //std::clog << std::format("Open Size: {} x {}\n", image->rows, image->cols);
 
         // HISTOGRAM WORK
 
@@ -40,20 +41,23 @@ int main(const int argc, char** argv) {
         cv::waitKey(0);
 
         // LOG MOD
+        if (parser->is_used("-l")) {
+            const auto c = parser->get<int>("-l");
 
-        std::unique_ptr<HistogramModifier> modifier = std::make_unique<LogHistogramModifier>(image, 25); // TODO: var for const 25
-        image = modifier->Modify();
+            std::unique_ptr<HistogramModifier> modifier = std::make_unique<LogHistogramModifier>(image, c); // TODO: var for const 25
+            image = modifier->Modify();
 
-        hist->SetImage(image);
-        visualizer = std::make_unique<HistogramVisualizer>(hist);
+            hist->SetImage(image);
+            visualizer = std::make_unique<HistogramVisualizer>(hist);
 
-        visualizer->PlotHistogram();
-        cv::waitKey(0);
+            visualizer->PlotHistogram();
+            cv::waitKey(0);
 
-        cv::imshow("After", *image);
-        cv::waitKey(0);
+            cv::imshow("After", *image);
+            cv::waitKey(0);
 
-        cv::imwrite(std::format("mod_{}", filePath), *image);
+            cv::imwrite(std::format("mod_{}", filePath), *image);
+        }
 
         return EXIT_SUCCESS;
     }
